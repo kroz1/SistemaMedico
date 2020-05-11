71419971b60bdf1ce3b40d26d97275c6356f3499
EdgarRojas <edgarrojas13@hotmail.com>
2020-05-11 15:09:44 -0500

    Se agregaron las opciones del menu, falto de terminar la codificacion del apartado de medicos y especialidades

---
+++ SistemaMedico/Controllers/MedicosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SistemaMedico.cModels;
+using SistemaMedico.Models;
+using Newtonsoft.Json;
+
+namespace SistemaMedico.Controllers
+{
+    public class MedicosController : Controller
+    {
+        public Models.citas_medicasEntities db = new Models.citas_medicasEntities();
+        // GET: Medicos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
