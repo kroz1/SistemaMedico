4fc79e75fab1be823c78cfa759378975ddbb02d8
EdgarRojas <edgarrojas13@hotmail.com>
2020-10-30 17:37:07 -0600

    Se realizan las operaciones de eliminar evetos en full calendar

--- SistemaMedico/Controllers/CalendarioController.cs
+++ SistemaMedico/Controllers/CalendarioController.cs
@@ -23,5 +23,52 @@ namespace SistemaMedico.Controllers
                 return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
             }
         }
+
+        [HttpPost]
+        public JsonResult SaveEvent(Events e)
+        {
+            var status = false;
+            using (citas_medicasEntities1 db = new citas_medicasEntities1())
+            {
+                if(e.Id > 0)
+                {
+                    //Actualizar el evento
+                    var v = db.Events.Where(a => a.Id == e.Id).FirstOrDefault();
+                    if(v != null)
+                    {
+                        v.Tema = e.Tema;
+                        v.FechaInicio = e.FechaInicio;
+                        v.FechaFin = e.FechaFin;
+                        v.Descripcion = e.Descripcion;
+                        v.EsDiaEntero = e.EsDiaEntero;
+                        v.Color = e.Color;
+                    }
+                }
+                else
+                {
+                    db.Events.Add(e);
+                }
+                db.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int IdEvent)
+        {
+            var status = false;
+            using (citas_medicasEntities1 dc = new citas_medicasEntities1())
+            {
+                var v = dc.Events.Where(a => a.Id == IdEvent).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Events.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
     }
 }
